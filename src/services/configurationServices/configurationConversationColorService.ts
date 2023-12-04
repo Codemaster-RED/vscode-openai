946e045000e7f46b0eb25be09a00f8a5539e2a94
Mac <red@codemaster.red>
2023-12-04 14:37:54 +0000

    background

--- src/services/configurationServices/configurationConversationColorService.ts
+++ src/services/configurationServices/configurationConversationColorService.ts
@@ -1,47 +1,56 @@
-import { window, ColorThemeKind } from 'vscode'
-import { createErrorNotification, createInfoNotification } from '@app/apis/node'
-import ConfigurationService from './configurationService'
-import { IConfigurationConversationColor } from '@app/types'
+import { window, ColorThemeKind } from "vscode";
+import {
+  createErrorNotification,
+  createInfoNotification,
+} from "@app/apis/node";
+import ConfigurationService from "./configurationService";
+import { IConfigurationConversationColor } from "@app/types";
 
 export default class ConfigurationConversationColorService
   extends ConfigurationService
   implements IConfigurationConversationColor
 {
-  private static _instance: ConfigurationConversationColorService
-  private readonly _cfg = 'conversation-configuration.colors.'
+  private static _instance: ConfigurationConversationColorService;
+  private readonly _cfg = "conversation-configuration.colors.";
   static get instance(): ConfigurationConversationColorService {
     if (!this._instance) {
       try {
-        this._instance = new ConfigurationConversationColorService()
+        this._instance = new ConfigurationConversationColorService();
       } catch (error) {
-        createErrorNotification(error)
+        createErrorNotification(error);
       }
     }
-    return this._instance
+    return this._instance;
   }
 
   public get userColor(): string {
     return this._isThemeDark()
       ? this.getConfigValue<string>(`${this._cfg}darkUserColor`)
-      : this.getConfigValue<string>(`${this._cfg}lightUserColor`)
+      : this.getConfigValue<string>(`${this._cfg}lightUserColor`);
   }
 
   public get userBackground(): string {
     return this._isThemeDark()
       ? this.getConfigValue<string>(`${this._cfg}darkUserBackground`)
-      : this.getConfigValue<string>(`${this._cfg}lightUserBackground`)
+      : this.getConfigValue<string>(`${this._cfg}lightUserBackground`);
   }
 
   public get assistantColor(): string {
     return this._isThemeDark()
       ? this.getConfigValue<string>(`${this._cfg}darkAssistantColor`)
-      : this.getConfigValue<string>(`${this._cfg}lightAssistantColor`)
+      : this.getConfigValue<string>(`${this._cfg}lightAssistantColor`);
   }
 
   public get assistantBackground(): string {
     return this._isThemeDark()
       ? this.getConfigValue<string>(`${this._cfg}darkAssistantBackground`)
-      : this.getConfigValue<string>(`${this._cfg}lightAssistantBackground`)
+      : this.getConfigValue<string>(`${this._cfg}lightAssistantBackground`);
+  }
+
+  public get assistantBackgroundImage(): string {
+    return this._isThemeDark()
+      ? this.getConfigValue<string>(`${this._cfg}darkBackgroundImage`)
+      : this.getConfigValue<string>(`${this._cfg}lightBackgroundImage`);
   }
 
   private _isThemeDark() {
@@ -50,21 +59,22 @@ export default class ConfigurationConversationColorService
       [ColorThemeKind.Dark]: true,
       [ColorThemeKind.HighContrast]: true,
       [ColorThemeKind.HighContrastLight]: false,
-    }[window.activeColorTheme.kind]
+    }[window.activeColorTheme.kind];
   }
 
   public static LogConfigurationService(): void {
     try {
-      const cfg = this.instance
-      const cfgMap = new Map<string, string>()
-      cfgMap.set('userColor', cfg.userColor)
-      cfgMap.set('userBackground', cfg.userBackground)
-      cfgMap.set('assistantColor', cfg.assistantColor)
-      cfgMap.set('assistantBackground', cfg.assistantBackground)
+      const cfg = this.instance;
+      const cfgMap = new Map<string, string>();
+      cfgMap.set("userColor", cfg.userColor);
+      cfgMap.set("userBackground", cfg.userBackground);
+      cfgMap.set("assistantColor", cfg.assistantColor);
+      cfgMap.set("assistantBackground", cfg.assistantBackground);
+      cfgMap.set("assistantBackgroundImage", cfg.assistantBackgroundImage);
 
-      createInfoNotification(Object.fromEntries(cfgMap), 'conversation_color')
+      createInfoNotification(Object.fromEntries(cfgMap), "conversation_color");
     } catch (error) {
-      createErrorNotification(error)
+      createErrorNotification(error);
     }
   }
 }
--- webview-ui/messageWebview/src/index.tsx
+++ webview-ui/messageWebview/src/index.tsx
@@ -2,20 +2,31 @@ import {
   webDarkTheme,
   webLightTheme,
   FluentProvider,
-} from '@fluentui/react-components'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+  tokens,
+} from "@fluentui/react-components";
+import React, { CSSProperties } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const rootElement = document.getElementById('root') as HTMLElement
+const rootElement = document.getElementById("root") as HTMLElement;
 const theme =
-  rootElement.getAttribute('theme') === 'dark' ? webDarkTheme : webLightTheme
-const root = ReactDOM.createRoot(rootElement)
+  rootElement.getAttribute("theme") === "dark" ? webDarkTheme : webLightTheme;
+const root = ReactDOM.createRoot(rootElement);
 
+const re = document.getElementById("root") as HTMLElement;
+const backgroundImage =
+  re.getAttribute("backgroundImage") ?? tokens.colorNeutralBackground4;
+
+const styleCode: CSSProperties = {
+  backgroundImage: `url(${backgroundImage})`,
+  backgroundSize: "contain",
+  backgroundRepeat: "repeat-y",
+  minHeight: "100vh",
+};
 root.render(
   <React.StrictMode>
-    <FluentProvider theme={theme}>
+    <FluentProvider theme={theme} style={styleCode}>
       <App />
     </FluentProvider>
   </React.StrictMode>
-)
+);
